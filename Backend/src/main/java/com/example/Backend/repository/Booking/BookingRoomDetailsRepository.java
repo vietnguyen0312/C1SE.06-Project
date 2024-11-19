c59623e4711db36a490595d9defa3c32b502646d
Bùi Văn Bình <buivanbinh1607@gmail.com>
2024-11-19 19:53:56 +0700

    fix history booking room (2)

--- Backend/src/main/java/com/example/Backend/repository/Booking/BookingRoomDetailsRepository.java
+++ Backend/src/main/java/com/example/Backend/repository/Booking/BookingRoomDetailsRepository.java
@@ -6,54 +6,50 @@ import com.example.Backend.entity.Booking.BookingRoomDetails;
 import com.example.Backend.entity.Room.Room;
 
 import com.example.Backend.entity.Room.RoomType;
-<<<<<<< Updated upstream
-=======
 import com.example.Backend.entity.Service.ServiceEntity;
 import feign.Param;
->>>>>>> Stashed changes
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-<<<<<<< Updated upstream
-import org.springframework.data.repository.query.Param;
-=======
->>>>>>> Stashed changes
 
 import java.time.Instant;
 import java.util.List;
 
 public interface BookingRoomDetailsRepository extends JpaRepository<BookingRoomDetails, String> {
-    List<BookingRoomDetails> findAllByBookingRoom(BookingRoom bookingRoom);
+        List<BookingRoomDetails> findAllByBookingRoom(BookingRoom bookingRoom);
 
-    List<BookingRoomDetails> findByRoom(Room room);
+        List<BookingRoomDetails> findByRoom(Room room);
 
-    Page<BookingRoomDetails> findByBookingRoom_User_Email(String email, Pageable pageable);
+        Page<BookingRoomDetails> findByBookingRoom_User_Email(String email, Pageable pageable);
 
-    List<BookingRoomDetails> findByBookingRoom_Id(String id, Sort sort);
+        List<BookingRoomDetails> findByBookingRoom_Id(String id, Sort sort);
 
-    List<BookingRoomDetails> findByBookingRoom_IdAndRoom_RoomType(String idBookingRoom, RoomType roomType, Sort sort);
+        List<BookingRoomDetails> findByBookingRoom_IdAndRoom_RoomType(String idBookingRoom, RoomType roomType,
+                        Sort sort);
 
-    @Query("SELECT new com.example.Backend.dto.response.RoomTypeRevenueResponse(b.room.roomType, SUM(b.price)) " +
-            "FROM BookingRoomDetails b " +
-            "WHERE b.bookingRoom.checkInDate BETWEEN :startDate AND :endDate " +
-            "GROUP BY b.room.roomType " +
-            "ORDER BY SUM(b.price) DESC")
-    Page<RoomTypeRevenueResponse> findTopRoomTypeByRevenue(
-            @Param("startDate") Instant startDate,
-            @Param("endDate") Instant endDate,
-            Pageable pageable);
-    List<BookingRoomDetails> findByCheckOutedIsNullAndCheckInedIsNotNull();
+        @Query("SELECT new com.example.Backend.dto.response.RoomTypeRevenueResponse(b.room.roomType, SUM(b.price)) " +
+                        "FROM BookingRoomDetails b " +
+                        "WHERE b.bookingRoom.checkInDate BETWEEN :startDate AND :endDate " +
+                        "GROUP BY b.room.roomType " +
+                        "ORDER BY SUM(b.price) DESC")
+        Page<RoomTypeRevenueResponse> findTopRoomTypeByRevenue(
+                        @Param("startDate") Instant startDate,
+                        @Param("endDate") Instant endDate,
+                        Pageable pageable);
 
-    @Query("SELECT b FROM BookingRoomDetails b " +
-            "JOIN b.bookingRoom br " +
-            "WHERE " +
-            "((b.checkOuted IS NULL AND b.checkIned IS NOT NULL) " +
-            "OR (b.checkOuted IS NULL AND br.checkOutDate > CURRENT_TIMESTAMP AND br.checkInDate < CURRENT_TIMESTAMP) " +
-            "OR (b.checkOuted IS NULL AND b.checkIned IS NULL AND br.checkOutDate > CURRENT_TIMESTAMP AND br.checkInDate < CURRENT_TIMESTAMP) " +
-            "OR (DATE(br.checkInDate) = CURRENT_DATE AND DATE(br.checkOutDate) = CURRENT_DATE))")
-    List<BookingRoomDetails> findActiveBookingRoomDetails();
+        List<BookingRoomDetails> findByCheckOutedIsNullAndCheckInedIsNotNull();
 
+        @Query("SELECT b FROM BookingRoomDetails b " +
+                        "JOIN b.bookingRoom br " +
+                        "WHERE " +
+                        "((b.checkOuted IS NULL AND b.checkIned IS NOT NULL) " +
+                        "OR (b.checkOuted IS NULL AND br.checkOutDate > CURRENT_TIMESTAMP AND br.checkInDate < CURRENT_TIMESTAMP) "
+                        +
+                        "OR (b.checkOuted IS NULL AND b.checkIned IS NULL AND br.checkOutDate > CURRENT_TIMESTAMP AND br.checkInDate < CURRENT_TIMESTAMP) "
+                        +
+                        "OR (DATE(br.checkInDate) = CURRENT_DATE AND DATE(br.checkOutDate) = CURRENT_DATE))")
+        List<BookingRoomDetails> findActiveBookingRoomDetails();
 
 }
